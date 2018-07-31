3b057ae0ff7aaad2ae419fbbbb6c1167f86a5405
toanpn190 <toanpn190@gmail.com>
2018-08-01 06:12:39 +0700

    Add empty service worker

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+//
+// Registering service worker
+//
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+//
+// Main app
+//
 const States = {
   OFF: 0,
   ON: 1,
